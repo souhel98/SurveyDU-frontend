695679abd9d6f09f8e85c7c01bfbbfcd31c13a9d
Souhel <sudkisouhel@gmail.com>
2025-07-25 19:32:16 +0300

    fix layout 2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: process.env.NODE_ENV === 'production' ? '/SurveyDU' : '',
     images: {
         unoptimized: true,
     },
